869c9da20b6bd5db7852f5d2ea33c3e563f3eb3d
Lewis Kai Ho Wong (k22027157) <lewis.wong@kcl.ac.uk>
2023-09-26 13:29:36 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,19 @@
 
 def primes(number_of_primes):
     list = []
+    listOfPrimes = []
+    number = 2
+    if (number_of_primes<1):
+        raise ValueError
+    while (len(listOfPrimes) < number_of_primes):
+        check = True
+        for a in range(1, math.ceil(number ** 0.5) + 1):
+            if a == 1 or a == number:
+                continue
+            if (number % a == 0):
+                check = False
+        if check:
+            listOfPrimes.append(number)
+        number += 1
     return list
+
